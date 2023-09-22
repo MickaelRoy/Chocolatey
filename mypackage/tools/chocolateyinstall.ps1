0d7bcddbe5b705a5b66879dc742285edc4833fef
Mickael ROY <mickael.roy.ext@boursorama.fr>
2023-09-22 14:29:55 +0200

    push mypackage full tricky details

---
+++ mypackage/tools/chocolateyinstall.ps1
@@ -0,0 +1,7 @@
+﻿$ErrorActionPreference = 'Stop'
+
+#Variable
+$packagename ='mypackage'
+#$installerType=''
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$PostInstallTask = Get-ScheduledTask -TaskName 'Citrix VDA post installation' -TaskPath \ -ea 0 -InformationAction SilentlyContinue
